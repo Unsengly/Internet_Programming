3cbef20d45fc6eaf98836fc43a90b90af5ac4a51
Un Sengly <131955066+Unsengly@users.noreply.github.com>
2023-12-15 18:20:09 +0700

    TP02

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="app">
+        <router-view />
+    </div>
+</template>
+
+<script>
+import { RouterView } from 'vue-router';
+export default {
+    name: 'App',
+    components: {
+        RouterView,
+    }
+}
+</script>
+
+<style>
+.app {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
